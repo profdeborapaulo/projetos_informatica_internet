44c59e2018070136980ac09b5f9d25a6ed9c36db
vinicius-dec <viniciusbellonidesouza@gmail.com>
2025-11-24 08:30:40 -0300

    Add files via upload

---
+++ segundo-info/projeto-final/Vinicius_Belloni_Projeto-FarMed/server.js
@@ -0,0 +1,16 @@
+import "dotenv/config"
+import app from "./back-end/app.js"
+
+const PORT = 3000;
+
+const rotas = {
+    "/": "Back-ed",
+    "/Categorias":"Entrei na area de categoria"
+};
+
+
+
+app.listen(PORT, () =>{
+    console.log("Servidor escutando...");   
+});
+
