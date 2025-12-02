77cc2e7e4a5628e7acf105c481930e7485b2709d
Lucas Macena <139016812+LucasMacena09@users.noreply.github.com>
2025-12-02 16:23:32 -0300

    Delete segundo-info/projeto-final/LucasMacena-TransMed directory

--- segundo-info/projeto-final/LucasMacena-TransMed/supabase/migrations/20251123230226_436dcf05-2e44-4f42-bbf4-e8069e58e611.sql
+++
@@ -1,4 +0,0 @@
--- Add quantity column to expenses table for tracking number of bus passes
-ALTER TABLE expenses ADD COLUMN quantity INTEGER DEFAULT 1;
-
-COMMENT ON COLUMN expenses.quantity IS 'Quantidade de passagens (usado principalmente para ônibus)';
